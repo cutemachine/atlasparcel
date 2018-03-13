8cebea8ead266e3b99c7d7cc045e2a069a1b350f
Jo Meenen <jo@meenen.de>
2018-03-13 08:01:17 +0100

    List the most important command on the CommandsPage

--- src/pages/CommandsPage.js
+++ src/pages/CommandsPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import MainSection from '../components/MainSection'
 import ContentWrapper from '../components/ContentWrapper'
 import PageTitle from '../components/PageTitle'
 
@@ -8,7 +7,20 @@ export default class CommandsPage extends Component {
     return (
       <ContentWrapper>
         <PageTitle>Commands</PageTitle>
-        <MainSection />
+        <h2>Start</h2>
+        <p>You can start a development server with …</p>
+        <pre>yarn start</pre>
+        <h2>Build & Deploy</h2>
+        <p>You can build the project with the build command.</p>
+        <pre>yarn build</pre>
+        <p>This will create a folder named dist. If you have Netlify installed, you can deploy your application to Netlify with the following command.</p>
+        <pre>netlify deploy</pre>
+        <h2>Lint</h2>
+        <p>You can lint the project's source code with the following command.</p>
+        <pre>yarn lint</pre>
+        <h2>Fixing Linter Errors</h2>
+        <p>You can fix linter errors with the following command.</p>
+        <pre>yarn lint-fix</pre>
       </ContentWrapper>
     )
   }
